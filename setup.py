108058821e320dc2b9b67d7ce531a049f2e4d339
Pavol Ipoth <pavol.ipoth@telekom.com>
2018-08-28 10:48:53 +0200

    Raising version of PI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import stat
 import sys
 
 #VERSION="2.1dev4"
-VERSION="2.22-pannet-4"
+VERSION="2.22-pannet-5"
 
 # Taken from kennethreitz/requests/setup.py
 package_directory = os.path.realpath(os.path.dirname(__file__))
